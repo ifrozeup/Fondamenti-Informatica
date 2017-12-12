cf61072563549820bf0d5a6e7b2c071bf41d8ac0
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2017-12-12 16:18:57 +0100

    Add files via upload

---
+++ Programs/23_PTS.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    FILE* filepts;
+
+
+    return 0;
+}
+
+void readFile(FILE* filepts){
+
+    int num;
+
+    if(isPrime(num))
+        isPTS(num);
+}
+
+void isPTS(int num){
+
+    addToList(num);
+
+}
+
+int isPrime(int num){
+
+}
+
+void addToList(int num){
+
+}
