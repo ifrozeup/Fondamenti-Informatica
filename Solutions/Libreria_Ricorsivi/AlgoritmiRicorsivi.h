45bcfec94666325b5b234f6b241d161e50d62192
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2017-12-23 17:58:09 +0100

    Add files via upload

---
+++ Solutions/Libreria_Ricorsivi/AlgoritmiRicorsivi.h
@@ -0,0 +1,16 @@
+/*
+ * AlgoritmiRicorsivi.h
+ *
+ * Libreria per la definizione di una serie di algoritmi ricorsivi che operano su liste di tipo intero
+ */
+
+#ifndef ALGORITMIRICORSIVI_H_
+#define ALGORITMIRICORSIVI_H_
+
+#include "FunzioniListe.h"
+
+void sommaLista(ListaNodi);
+void cancellaValore(ListaNodi*, int);
+void cancellaRipetizioni(ListaNodi*);
+
+#endif /* ALGORITMIRICORSIVI_H_ */
