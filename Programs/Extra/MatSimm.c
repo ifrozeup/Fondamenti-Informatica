042553ba92f48dfb00573c270dd4637db482f6f5
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-01-24 00:49:22 +0100

    Update MatSimm.c

--- Programs/Extra/MatSimm.c
+++ Programs/Extra/MatSimm.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include <math.h>
+#define N 4
 
-int n = 4;
+int simm(int** mat, int n);
 
 int main(){
     int i, j;
 
-    int** matrix = malloc(n*sizeof(int*));
-    
-    for(i=0; i<n; i++)
+    int** matrix = malloc(N*sizeof(int*));
+    for(i=0; i<N; i++)
     {
-        matrix[i]=malloc(n*sizeof(int));
+        matrix[i]=malloc(N*sizeof(int));
 
-        for(j=0; j<n; j++) //genero casualmente
+        for(j=0; j<N; j++) //genero casualmente
         {
             if(i!=j)
             {
@@ -23,25 +23,24 @@ int main(){
         }
     }
 
-    for(i=0; i<n; i++) //rendo simmetrica
+    for(i=0; i<N; i++) //rendo simmetrica
     {
-        for(j=0; j<n; j++)
+        for(j=0; j<N; j++)
         {
             if(i!=j)
                 matrix[i][j] = matrix[j][i];
         }
     }
-
-    int ris = simm(matrix);
+    int ris = simm(matrix, N);
 
     return ris;
 }
 
-int simm(int** mat){
+int simm(int** mat, int n){
 
     int i,j, ok = 1;
 
-    for(i=0; i<n; i++) //controllo uguaglianza vettori esterni membro a membro
+    for(i=0; i<n; i++)
     {
         if(mat[n-1][i] != mat[i][n-1])
             ok = 0;
@@ -50,7 +49,7 @@ int simm(int** mat){
     if(ok && n>1)
     {
         n--;
-        return simm(mat);
+        return simm(mat, n);
     }
     else if(ok)
         return 1;
