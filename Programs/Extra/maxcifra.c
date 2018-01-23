1225bd7a4240e4d92bfe7dcd456915451a7439eb
Francesco Puddu <34393627+ifrozeup@users.noreply.github.com>
2018-01-23 16:52:40 +0100

    Add files via upload

---
+++ Programs/Extra/maxcifra.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int massimo = max(123851214);
+
+    printf("\n%d", massimo);
+
+    return 0;
+}
+
+int max(int n)
+{
+
+    int mass;
+    mass = n%10;
+
+        while(n>10)
+        {
+            n = n/10;
+            printf("\n%d", n);
+            if(mass<n%10)
+               return max(n);
+        }
+    return mass;
+}
